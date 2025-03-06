e49864182bf85d89a5da705a854cc71b902e68b2
gulnazkyzy <gulnazkyzy@yahoo.com>
2025-03-06 20:50:26 +0000

    Added class tasks file

---
+++ Session1/Class_task.py
@@ -0,0 +1,44 @@
+word = "Hello World"
+# print(word)
+
+# num1 = input("Please provide number 1: ")
+# num2 = input("Please provide number 2: ")
+# print(num1 + num2)
+
+# print("Converted to integer")
+# num1 = int(num1)
+# num2 = int(num2)
+
+# print("4" + "5")
+# print(type(num1), type(num2))
+# name = input("Please provide your name: ")
+# print("Hello " + name)
+
+# Class task1
+
+# input1 = float(input("Please provide number 1: "))
+# input2 = float(input("Please provide number 2: "))
+# answer = (input1 + input2) / 2
+# print(answer)
+
+
+# Indexing | Slicing strings
+# word = "Hello"
+# print(word[0])
+# print(word[0] + word[5])
+# print(word[-1])
+# print(word[1:5])  #ello
+# 012345
+# "Hello"
+# -5-4-3-2-1
+
+# Class task number 3:
+
+# word = "Hello from aKumoSolutions"
+# print(word[11:25])
+# slice the word: aKumoSolutions
+
+
+# Integer 
+# Math Equations
+
